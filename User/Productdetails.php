c32d11e6c88f604e9d75a650ca1d62428cda908f
manoj <manojpoojary2004@gmail.com>
2024-06-10 21:44:44 +0530

    Update

--- User/Productdetails.php
+++ User/Productdetails.php
@@ -6,16 +6,28 @@ $server = "127.0.0.1:3307";
 $username = "root";
 $password = "";
 $database = "admin";
-$conn=mysqli_connect($server,$username,$password,$database);
+$conn = mysqli_connect($server, $username, $password, $database);
 
-$requestType=$_SERVER["REQUEST_METHOD"];
-$type=$_GET['type'];
+$requestType = $_SERVER["REQUEST_METHOD"];
+$type = $_GET['type'];
 
-if ($requestType=="POST" && $type=="products") {
+if ($requestType == "POST" && $type == "products") {
     header("Content-Type: application/json");
-    $product_accessed_id=file_get_contents("php://input");
-    $decoded_product_id=json_decode($product_accessed_id,true);
-    $id=$decoded_product_id['Id'];
-    echo json_encode($id);
+    $product_accessed_id = file_get_contents("php://input");
+    $decoded_product_id = json_decode($product_accessed_id, true);
+    $id = $decoded_product_id['Id'];
+    // echo json_encode($id);
+
+    $product_data_fetch_sql = "SELECT * FROM productdetails,shopdetails where pid=$id";
+    $result = mysqli_query($conn, $product_data_fetch_sql);
+    if (mysqli_num_rows($result)>0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $products_data[] = $row;
+        }
+        echo json_encode($products_data);
+    }
+    else{
+        echo json_encode("No data found");
+    }
 }
 ?>
