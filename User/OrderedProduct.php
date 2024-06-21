5e967508e6c521dd22d37f2e3f3e5eb727e2b8b9
manoj <manojpoojary2004@gmail.com>
2024-06-21 07:31:30 +0530

    order details action

--- User/OrderedProduct.php
+++ User/OrderedProduct.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Headers:Content-Type");
 $server = "127.0.0.1:3307";
 $username = "root";
 $password = "";
-$database = "admin";
+$database = "user";
 $conn = mysqli_connect($server, $username, $password, $database);
 
 $requestType = $_SERVER["REQUEST_METHOD"];
@@ -14,8 +14,27 @@ $type = $_GET['type'];
 if ($requestType == "POST" && $type == "ordered_products") {
     header("Content-Type: application/json");
     $product_accessed_id = file_get_contents("php://input");
-    $decoded_product_id = json_decode($product_accessed_id, true);
-    // echo $decoded_product_id['userAddress'];
-    print_r(json_encode(array("msg"=> $decoded_product_id['userAddress'])));
+    $orderedData = json_decode($product_accessed_id, true);
+
+    $useraddress=$orderedData['userAddress'];
+    $userContactDetails=$orderedData['userContactDetails'];
+    $orderedTime=$orderedData['ordered_Time'];
+    $orderedDate=$orderedData['ordered_Date'];
+    $orderedUserId=$orderedData['userId'];
+    $orderedShopId=$orderedData['shopId'];
+    $orderedProductName=$orderedData['productName'];
+    $orderedProductPrice=$orderedData['productPrice'];
+    $orderedProductQty=$orderedData['productQty'];
+
+    $insertSql = "INSERT INTO orderdetails (`useraddress`, `userContactDetails`, `orderedTime`, `orderedDate`,`userid`,`shopid`,`productname`,`productprice`,`productqty`) 
+    VALUES ('$useraddress', '$userContactDetails', '$orderedTime', '$orderedDate', '$orderedUserId','$orderedShopId','$orderedProductName','$orderedProductPrice','$orderedProductQty')";
+    $connect_query=mysqli_query($conn,$insertSql);
+    if ($connect_query) {
+        print_r(json_encode(array("msg"=> "ordered product saved successfully")));
+
+    }else{
+        print_r(json_encode(array("msg"=> "Error")));
+
+    }
 }
 ?>
